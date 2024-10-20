fab800221b673b374aadf587175c9a95be6afb93
Anurag Aman <coding.demon123@gmil.com>
2024-10-20 13:38:03 +0530

    write a function of sum and sub in diffrent js file and export it in index.js file

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("Anurag Bhardwaj");
-console.log("hello world");
+const math=require("./math");
+
+const ans1=math.sub(4,5);
+const ans2=math.sum(4,3);
+
+
+console.log(ans1);
+console.log(ans2);
+
