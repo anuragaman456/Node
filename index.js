fcea81948e9359ac01636eaa6d2245a404792d7f
Anurag Aman <coding.demon123@gmil.com>
2024-10-21 10:08:49 +0530

    In http server we added a feature when user send request then timestamp and newrequest write on new log.txt file

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const http=require("http");
+const fs=require("fs");
+
 
 const myserver=http.createServer((req,res)=>{
       console.log("New request reached");
-      console.log(req.headers);
-      res.end("Heloo My Name is Anurag Aman");
+    //   console.log(req.headers);
+    const log= `${Date.now()} : New Request\n`
+    fs.appendFile("log.txt",log,(err)=>{
+       if(err)
+            console.log("Errorfound!");
+       else
+           res.end("This is the resposnse that i am sending to user");         
+    });
+     
 });
 
 myserver.listen(8001,()=>{
