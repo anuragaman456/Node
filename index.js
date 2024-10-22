e61ccb2885e7dfad81f3ea15be19f2db8174e014
Anurag Aman <coding.demon123@gmil.com>
2024-10-22 12:32:42 +0530

    upate in app use express frame work and increase the version to 1.1.0

--- index.js
+++ index.js
@@ -1,60 +1,20 @@
-const http=require("http");
-const fs=require("fs");
-const url=require("url");
+const http =require("http");
+const express=require("express");
 
+const app=express();
 
-const myserver=http.createServer((req,res)=>{
-      console.log("New request reached");
-    //   console.log(req.headers);
-    if(req.url==="/favicon.ico")
-        return res.end();
-    const log= `${Date.now()} : ${req.url}:method:${req.method} :New Request\n`
-    if(req.url===" /favicon.ico")
-        return res.end();
-
-    const myurl=url.parse(req.url,true);
-    console.log(myurl);
-    fs.appendFile("log.txt",log,(err)=>{
-       if(err)
-            console.log("Errorfound!");
-       else{    
-             const name=myurl.query.username;
-            switch(myurl.pathname)
-            {
-                case "/":
-                    if(req.method==="GET"){ 
-                        res.end("Homepage");
-                        break;
-                    }
-                case "/about":
-                    res.end(`hlw ,${name}`);
-                    break;
-
-                // here i am writing the case of how youtube ,google works
-                case "/search":
-                    const result=myurl.query.search_query;
-                    res.end(`Here are your results for ${result}`);  
-                    break;
-
-                case"/signup":
-                 if(req.method==="GET")
-                 {
-                    res.end("this is signup page");
-                 }
-                 else if(req.method==="POST")
-                 {
-                    res.end("sucessfully signup");
-                 }
-
-                default:
-                    res.end("404 route Not found");
-                    
-            } 
-        }     
-    });
-     
+app.get("/",(req,res)=>
+{
+    console.log("new reaquest reached");
+    return res.send("hey there");
 });
 
-myserver.listen(8001,()=>{
-    console.log("server started");
+app.get("/about",(req,res)=>{
+    console.log(req.query.username);
+
+    return res.send("This is about page"+req.query.username);
+    
 });
+
+
+app.listen(8000,()=>{console.log("your server started")});
