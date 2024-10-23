27f581bc78ca284de19fde388b39b6b9ae15d598
Anurag Aman <coding.demon123@gmil.com>
2024-10-24 01:59:08 +0530

    created get,post,patch and delete Restfull api

--- index.js
+++ index.js
@@ -29,13 +29,14 @@ app.get("/api/users/:id",(req,res)=>
 });
 
 app.get("/api/users",(req,res)=>{   // for good practise means agar koi mobile, flutter ,alexa
-                                    // se call mare to json format me chal jaye and browser se mare to html
+   // console.log("body",req.body)                          // se call mare to json format me chal jaye and browser se mare to html
     return res.json(users);         // render kr do server side rendering me
     
 });
 
 app.post("/api/users",(req,res)=>{
      const body=req.body;
+     console.log(body);
      users.push({...body,id:users.length+1});
      fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err)=>{
         if(err)
@@ -44,11 +45,57 @@ app.post("/api/users",(req,res)=>{
         }
         else
         {
-            res.send({status:"sucessfully Added"});
+            res.send({status:"sucessfully Added",id:users.length});
         }
-     })
+     });
+    });
+
+app.patch("/api/users/:id",(req,res)=>
+{
+    
+  const user_id=parseInt(req.params.id);
+  const user_index=users.findIndex((user)=>user.id===user_id);
+  const updated_data=req.body;
+  if(user_index==-1)
+  {
+    return res.send({status:"Error"});
+  }
+   
+  users[user_index]=({...users[user_index], ...updated_data});
+  console.log(`User ID: ${user_id}, User Index: ${user_index}`);
+
+
+  fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err)=>
+{
+     if(err)
+        res.send({status:"Eroor"});
+    else
+        res.send({status:"sucessfully updated or patched", id:user_id});
+});
 
 });
+
+app.delete("/api/users/:id",(req,res)=>{
+    const user_id=parseInt(req.params.id);
+    const index=users.findIndex((user)=>user.id===user_id);
+    if(index==-1)
+        res.send({status:"error"});
+    
+    else{
+           users.splice(index,1);
+        fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err)=>{
+           if(err)
+              res.send({status:"error"});
+           else
+              res.send({status:"Sucessfully_deleted",id:user_id});
+             
+        });
+    }
+    
+
+});
+
+
 
 
 
@@ -61,7 +108,9 @@ app.get("/api/about",(req,res)=>{
 });
 
 
-app.listen(8000,()=>{console.log("your server started")});
+app.listen(8000,()=>{console.log("your server started ");
+    
+});
 
 
 
